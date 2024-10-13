31e10839ab9bf887c9ec3f19d27356e3bd3cc97e
A-Rush-R <arush.up@gmail.com>
2024-10-13 12:11:28 +0530

    plot_history

--- common/evaluate.py
+++ common/evaluate.py
@@ -25,3 +25,27 @@ def evaluate_predictions(y_true, y_pred, title, ax=None):
     
     print(f'Accuracy is {accuracy_score(y_true,y_pred)}')
     print(f'=========={title}==========')
+    
+def plot_history(history):
+    # Plot loss
+    plt.figure(figsize=(12, 4))
+    
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['loss'], label='Train Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Loss over Epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['accuracy'], label='Train Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.title('Accuracy over Epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    
+    plt.tight_layout()
+    plt.show()
