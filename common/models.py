fcb9bc010c7156beb681b4b4c5346d3ff9bffc18
A-Rush-R <arush.up@gmail.com>
2024-10-14 11:39:30 +0530

    grid_search_functionality

--- common/models.py
+++ common/models.py
@@ -7,84 +7,146 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import GridSearchCV
 from param_grid import param_grids
 
-def predict_xgboost(x_train, y_train, x_val, use_label_encoder=False, eval_metric='logloss') :
+def predict_xgboost(x_train, y_train, x_val,grid_search : bool = False, use_label_encoder=False, eval_metric='logloss') :
+    '''
+    inputs : training data and validation data in dataframe
+    outputs : validation predictions and truth values
+    '''
+    if grid_search :
+        xgb_model = XGBClassifier(use_label_encoder=use_label_encoder, eval_metric=eval_metric)
+        best_params = grid_search_(xgb_model, x_train, y_train, model_name='xgb')
+        
+        xgb_model = XGBClassifier(use_label_encoder=use_label_encoder, **best_params)
+        xgb_model.fit(x_train, y_train)
+        
+        y_pred = xgb_model.predict(x_val)
+
+        return y_pred
+    else :
+        xgb_model = XGBClassifier(use_label_encoder=use_label_encoder, eval_metric=eval_metric)
+        xgb_model.fit(x_train, y_train)
+        
+        y_pred = xgb_model.predict(x_val)
+
+        return y_pred
+
+
+def predict_logistic_regression(x_train, y_train, x_val,grid_search : bool = False, max_iter=1000, random_state=42) :
+    '''
+    inputs : training data and validation data in dataframe
+    outputs : validation predictions and truth values
+    '''
+    if grid_search :
+        lr_model = LogisticRegression(max_iter=max_iter, random_state=random_state)
+        best_params = grid_search_(lr_model, x_train, y_train, model_name='lr')
+        
+        lr_model = LogisticRegression(**best_params)
+        lr_model.fit(x_train, y_train)
+        
+        y_pred = lr_model.predict(x_val)
+
+        return y_pred
+    else :
+        lr_model = LogisticRegression(max_iter=max_iter, random_state=random_state)
+        lr_model.fit(x_train, y_train)
+        
+        y_pred = lr_model.predict(x_val)
+
+        return y_pred
+
+def predict_random_forest(x_train, y_train, x_val, grid_search : bool = False, n_estimators=100, random_state=42, min_samples_leaf=1, max_features='sqrt') :
     '''
     inputs : training data and validation data in dataframe
     outputs : validation predictions and truth values
     '''
     
-    xgb_model = XGBClassifier(use_label_encoder=use_label_encoder, eval_metric=eval_metric)
-    xgb_model.fit(x_train, y_train)
-    
-    y_pred = xgb_model.predict(x_val)
+    if grid_search :
+        rf_model = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state, min_samples_leaf=min_samples_leaf, max_features=max_features)
+        best_params = grid_search_(rf_model, x_train, y_train, model_name='rf')
+        
+        rf_model = RandomForestClassifier(**best_params)
+        rf_model.fit(x_train, y_train)
+        
+        y_pred = rf_model.predict(x_val)
 
-    return y_pred
+        return y_pred
+    else :
+        rf_model = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state, min_samples_leaf=min_samples_leaf, max_features=max_features)
+        rf_model.fit(x_train, y_train)
+        
+        y_pred = rf_model.predict(x_val)
 
+        return y_pred
 
-def predict_logistic_regression(x_train, y_train, x_val, max_iter=1000, random_state=42) :
+def predict_mlp(x_train, y_train, x_val, grid_search : bool = False, max_iter=1000, random_state=42, hidden_layer_sizes=(100,50)) :
     '''
     inputs : training data and validation data in dataframe
     outputs : validation predictions and truth values
     '''
+    if grid_search :
+        mlp_model = MLPClassifier(max_iter=max_iter, random_state=random_state, hidden_layer_sizes=hidden_layer_sizes)
+        best_params = grid_search_(mlp_model, x_train, y_train, model_name='mlp')
+        
+        mlp_model = MLPClassifier(**best_params)
+        mlp_model.fit(x_train, y_train)
+        
+        y_pred = mlp_model.predict(x_val)
+        
+        return y_pred
     
-    lr_model = LogisticRegression(max_iter=max_iter, random_state=random_state)
-    lr_model.fit(x_train, y_train)
-    
-    y_pred = lr_model.predict(x_val)
+    else :
+        mlp_model = MLPClassifier(max_iter=max_iter, random_state=random_state, hidden_layer_sizes=hidden_layer_sizes)
+        mlp_model.fit(x_train, y_train)
+        
+        y_pred = mlp_model.predict(x_val)
 
-    return y_pred
+        return y_pred
 
-def predict_random_forest(x_train, y_train, x_val, n_estimators=100, random_state=42, min_samples_leaf=1, max_features='sqrt') :
+def predict_svc(x_train, y_train, x_val, grid_search : bool = False, random_state=42, kernel='rbf', c=1.0, gamma='scale', max_iter=1000) :
     '''
     inputs : training data and validation data in dataframe
     outputs : validation predictions and truth values
     '''
-    
-    rf_model = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state, min_samples_leaf=min_samples_leaf, max_features=max_features)
-    rf_model.fit(x_train, y_train)
-    
-    y_pred = rf_model.predict(x_val)
+    if grid_search :
+        svm_model = SVC(random_state=random_state, kernel=kernel, C=c, gamma=gamma, max_iter=max_iter)
+        best_params = grid_search_(svm_model, x_train, y_train, model_name='svm')
+        
+        svm_model = SVC(**best_params)
+        svm_model.fit(x_train, y_train)
+        
+        y_pred = svm_model.predict(x_val)
 
-    return y_pred
+        return y_pred
+    else :
+        svm_model = SVC(random_state=random_state, kernel=kernel, C=c, gamma=gamma, max_iter=max_iter)
+        svm_model.fit(x_train, y_train)
+        
+        y_pred = svm_model.predict(x_val)
 
-def predict_mlp(x_train, y_train, x_val, max_iter=1000, random_state=42, hidden_layer_sizes=(100,50)) :
+        return y_pred
+
+def predict_mnb(x_train, y_train, x_val, grid_search : bool = False, alpha=1.0, fit_prior=True) :
     '''
     inputs : training data and validation data in dataframe
     outputs : validation predictions and truth values
     '''
+    if grid_search :
+        mnb_model = MultinomialNB(alpha=alpha, fit_prior=fit_prior)
+        best_params = grid_search_(mnb_model, x_train, y_train, model_name='mnb')
+        
+        mnb_model = MultinomialNB(**best_params)
+        mnb_model.fit(x_train, y_train)
+        y_pred = mnb_model.predict(x_val)
+        
+        return y_pred
     
-    mlp_model = MLPClassifier(max_iter=max_iter, random_state=random_state, hidden_layer_sizes=hidden_layer_sizes)
-    mlp_model.fit(x_train, y_train)
-    
-    y_pred = mlp_model.predict(x_val)
+    else :
+        mnb_model = MultinomialNB(alpha=alpha, fit_prior=fit_prior)
+        mnb_model.fit(x_train, y_train)
+        
+        y_pred = mnb_model.predict(x_val)
 
-    return y_pred
-
-def predict_svc(x_train, y_train, x_val, random_state=42, kernel='rbf', c=1.0, gamma='scale', max_iter=1000) :
-    '''
-    inputs : training data and validation data in dataframe
-    outputs : validation predictions and truth values
-    '''
-    
-    svm_model = SVC(random_state=random_state, kernel=kernel, C=c, gamma=gamma, max_iter=max_iter)
-    svm_model.fit(x_train, y_train)
-    
-    y_pred = svm_model.predict(x_val)
-
-    return y_pred
-
-def predict_mnb(x_train, y_train, x_val) :
-    '''
-    inputs : training data and validation data in dataframe
-    outputs : validation predictions and truth values
-    '''
-    
-    mnb_model = MultinomialNB()
-    mnb_model.fit(x_train, y_train)
-    
-    y_pred = mnb_model.predict(x_val)
-
-    return y_pred
+        return y_pred
 
 def grid_search_(model, X_train, y_train, param_grid = None,model_name : str = None, cv = 5, scoring = 'accuracy') :
     '''
