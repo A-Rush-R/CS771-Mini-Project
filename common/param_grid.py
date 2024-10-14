9af005a449ccb7e10cc32f769c2405a18b0173d6
A-Rush-R <arush.up@gmail.com>
2024-10-14 16:32:16 +0530

    shorter grid

--- common/param_grid.py
+++ common/param_grid.py
@@ -1,27 +1,27 @@
 param_grids = {
     "xgb" : {
-        'n_estimators': [50, 100, 200, 500],
-        'max_depth': [3, 5, 7, 10],
-        'learning_rate': [0.01, 0.1, 0.3],
-        'subsample': [0.7, 0.8, 1.0],
-        'colsample_bytree': [0.7, 0.8, 1.0],
-        'gamma': [0, 0.1, 0.3],
-        'min_child_weight': [1, 3, 5],
+        'n_estimators': [50, 200, 500],
+        'max_depth': [3, 7],
+        'learning_rate': [0.01, 0.1],
+        'subsample': [0.7, 1.0],
+        'colsample_bytree': [0.7, 1.0],
+        'gamma': [0, 0.2],
+        'min_child_weight': [1, 3],
         'eval_metric': ['logloss', 'auc']
     },
     "lr" : {
         'penalty': ['l1', 'l2', 'elasticnet', 'none'],
-        'C': [0.01, 0.1, 1.0, 10.0, 100.0],
+        'C': [0.01, 1.0, 100.0],
         'solver': ['liblinear', 'saga', 'lbfgs'],
-        'max_iter': [100, 500, 1000],
+        'max_iter': [100, 1000],
         'fit_intercept': [True, False],
         'random_state': [42]
     },
     "rf" : {
-        'n_estimators': [100, 200, 500, 1000],
-        'max_depth': [None, 10, 20, 30],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 2, 4],
+        'n_estimators': [100, 500, 1000],
+        'max_depth': [None, 10, 30],
+        'min_samples_split': [2, 5],
+        'min_samples_leaf': [1, 4],
         'max_features': ['auto', 'sqrt', 'log2'],
         'bootstrap': [True, False],
         'random_state': [42]
@@ -30,15 +30,15 @@ param_grids = {
         'hidden_layer_sizes': [(50,50), (100,50), (100,100,50), (200,100)],
         'activation': ['tanh', 'relu'],
         'solver': ['adam', 'sgd'],
-        'alpha': [0.0001, 0.001, 0.01],
+        'alpha': [0.0001, 0.01],
         'learning_rate': ['constant', 'adaptive'],
-        'max_iter': [500, 1000, 2000],
+        'max_iter': [500, 2000],
         'random_state': [42]
     },
     "svc" : {
-        'C': [0.1, 1.0, 10, 100],
+        'C': [0.1, 10],
         'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
-        'degree': [2, 3, 4],
+        'degree': [2, 4],
         'gamma': ['scale', 'auto'],
         'max_iter': [1000, 5000],
         'random_state': [42]
