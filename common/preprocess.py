abcde7d1a9bbafaee9ce93de23f803751c7383a8
A-Rush-R <arush.up@gmail.com>
2024-10-20 12:37:17 +0530

    asf

--- common/preprocess.py
+++ common/preprocess.py
@@ -47,31 +47,7 @@ def one_hot_encode(train_df, valid_df):
     new_train_df = pd.DataFrame(oh_encoder.transform(new_train_df).toarray())
     new_valid_df = pd.DataFrame(oh_encoder.transform(new_valid_df).toarray())
     
-    print(new_train_df.shape, new_valid_df.shape)
-    
     return new_train_df, new_valid_df, y_train, y_val
-
-def find_common_characters(strings):
-    # Convert the first string to a set of characters
-    common_chars = set(strings[0])
-
-    # Intersect with characters from all other strings
-    for string in strings[1:]:
-        common_chars &= set(string)
-    
-    return common_chars
-
-def remove_common_characters(strings):
-    new_strings = []
-    
-    common_chars = find_common_characters(strings)
-    
-    for string in strings:
-        # Remove all common characters from the string
-        new_string = ''.join(char for char in string if char not in common_chars)
-        new_strings.append(new_string)
-
-    return new_strings
 
 
 def remove_substrings(input_string, substrings):
